3393b60ff3281403661ed9114878eaec78f8bba9
Nasraldeen Eisa <nsraldynaysa@hotmail.com>
2022-08-23 10:37:11 +0200

    Add files via upload
    CANDYtuft website template

---
+++ CANDYtuft/js/main.js
@@ -0,0 +1,6 @@
+// Toggle class spin icon
+document.querySelector("header .toggle-menu").onclick = function () {
+   // this.classList.toggle("menu-active");
+    // Toggle class open main setting box
+    document.querySelector("header .menu").classList.toggle("open");
+}
